549da46391bb60c0ced36edd26fe03365664965d
KaMeLoTmArMoT <andyshebeko@gmail.com>
2022-04-17 15:44:29 +0300

    add aes encryption for input data.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from Cryptodome.Cipher import AES
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager
 import sqlite3
 import hashlib
+import base64
 
 
 def call_db(call):
@@ -24,10 +26,18 @@ def call_db(call):
 
     return records
 
+
 def get_sha(text):
     enc = hashlib.sha256()
     enc.update(text.encode('utf-8'))
     return enc.hexdigest()
+
+
+def extend_key(text):
+    if len(text) < 16:
+        while len(text) < 16:
+            text += text
+    return text[:16].encode('utf-8')
 
 
 class BaseScreen:
@@ -35,11 +45,15 @@ class BaseScreen:
         """Put string message to the label"""
         self.ids.word_label.text = text
 
+    def get_input(self):
+        return self.ids.word_input.text
+
 
 class LoginScreen(Screen, BaseScreen):
-    def __init__(self, **kw):
-        super().__init__(**kw)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.passwords = ''
+        self.key = ''
 
     def on_enter(self, *args):
         self.create_db_and_check()
@@ -60,16 +74,16 @@ class LoginScreen(Screen, BaseScreen):
             self.ids.login.text = 'Register'
 
     def submit(self):
-        text = self.ids.word_input.text
+        self.key = self.get_input()
 
-        if len(text) <= 5:
+        if len(self.key) <= 5:
             self.label_out('Password should be longer than 5 letters.')
             return
 
         if len(self.passwords) == 0:
-            self.submit_new_password(text)
+            self.submit_new_password(self.key)
         else:
-            self.validate_password(text)
+            self.validate_password(self.key)
 
     def validate_password(self, inp_pass):
         real_value = self.passwords[0][1]
@@ -92,10 +106,17 @@ class LoginScreen(Screen, BaseScreen):
 
 
 class MainScreen(Screen, BaseScreen):
-    def __int__(self, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.key = None
 
     def on_enter(self, *args):
+        self.create_db_and_check()
+
+        key = self.manager.get_screen('login').key
+        self.key = extend_key(key)
+
+    def create_db_and_check(self):
         # Create a table
         call_db("""
         CREATE TABLE IF NOT EXISTS customers (
@@ -103,13 +124,17 @@ class MainScreen(Screen, BaseScreen):
         ) """)
 
     def submit(self):
-        text = self.ids.word_input.text
+        text = self.get_input()
 
         if len(text) <= 2:
             self.label_out('Text should be longer than 2 letters')
             return
 
-        call_db(f"INSERT INTO customers VALUES ('{text}')")
+        cipher = AES.new(self.key, AES.MODE_EAX, nonce=b'TODO')
+        encoded_text = cipher.encrypt(text.encode('utf-8'))
+        b_encoded_text = base64.b64encode(encoded_text).decode('utf-8')
+
+        call_db(f"INSERT INTO customers VALUES ('{b_encoded_text}')")
 
         # show message
         self.label_out(f'{text} Added')
@@ -122,7 +147,12 @@ class MainScreen(Screen, BaseScreen):
 
         words = ''
         for word in records:
-            words += f'{word[0]}\n'
+            cipher = AES.new(self.key, AES.MODE_EAX, nonce=b'TODO')
+
+            tm = word[0]
+            tm = base64.b64decode(tm.encode('utf-8'))
+            tm = cipher.decrypt(tm).decode('utf-8')
+            words += f'{tm}\n'
 
         self.label_out(words)
 
