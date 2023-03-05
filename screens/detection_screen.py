44254204aca5d86a4c51564f7a553a0bf9a59384
KaMeLoTmArMoT <andyshebeko@gmail.com>
2023-03-06 01:09:22 +0200

    implement preview from camera, add simple buttons (start/stop showing video), add cv2.CAP_DSHOW - for faster camera init

--- screens/detection_screen.py
+++ screens/detection_screen.py
@@ -1,7 +1,11 @@
 import os
 import subprocess
+import threading
+from functools import partial
 
 import cv2
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
 from kivy.uix.screenmanager import Screen
 
 from screens.additional import BaseScreen
@@ -16,14 +20,18 @@ class DetectionScreen(Screen, BaseScreen):
         self.app_folder = os.getcwd()
         self.projects_folder = os.path.join(self.app_folder, "projects_detection")
 
+        self.show_frames = False
+
     def on_enter(self, *args):
         self.ids.header.ids[self.manager.current].background_color = 1, 1, 1, 1
+
+        self.display_start()
 
     def init_camera(self) -> None:
         if self.camara is not None:
             return
 
-        self.camara = cv2.VideoCapture(0)
+        self.camara = cv2.VideoCapture(0, cv2.CAP_DSHOW)
         self.camara.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         self.camara.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
         self.camara.set(cv2.CAP_PROP_FPS, 30)
@@ -33,6 +41,35 @@ class DetectionScreen(Screen, BaseScreen):
         if self.camara is not None:
             self.camara.release()
             self.camara = None
+
+    def display_start(self):
+        self.show_frames = True
+        print("init")
+        self.init_camera()
+        print("start thread")
+        # TODO: make sure only 1 thread is alive (probably by self.show_frames)
+        threading.Thread(target=self.display_thread, daemon=True).start()
+        print("after thread")
+
+    def display_thread(self):
+        while self.show_frames:
+            ret, frame = self.camara.read()
+            print("call clock")
+            Clock.schedule_once(partial(self.display_frame, frame))
+
+    def display_frame(self, frame, tm, colorfmt="bgr"):
+        texture: Texture = Texture.create(
+            size=(frame.shape[1], frame.shape[0]), colorfmt=colorfmt
+        )
+        texture.blit_buffer(
+            frame.tobytes(order=None), colorfmt=colorfmt, bufferfmt="ubyte"
+        )
+        texture.flip_vertical()
+        self.ids.image.texture = texture
+        print("put texture")
+
+    def display_stop(self):
+        self.show_frames = False
 
     def labelimg_open(self) -> None:
         # TODO: make dynamic path for different projects
